d0b5c85e7c10e906529730de1afd81ad6b91f048
D2 <dsmontecastro1@up.edu.ph>
2023-09-13 17:18:20 +0800

    [-] Initial Commit

---
+++ engine.py
@@ -0,0 +1,262 @@
+import random
+
+
+def read_file(file):
+	"""	Returns a list of words given in a file.
+	"""
+
+	f = open(file)
+
+	words = [i.rstrip() for i in f]
+
+	return words
+
+def pick_word(dictionary, index):
+	"""	Returns the n-th word from a given dicitonary with n = index.
+	"""
+
+	return dictionary[index]
+
+def check_word(word, string):
+	"""	Returns True if a word can be formed from a string.
+		Otherwise, False.
+	"""
+
+	count = 0
+
+	for char in word:
+		if char in string:
+			index = string.index(char)
+			string = string[:index] + string[index + 1:]
+			count += 1
+			
+	if count == len(word):
+		return True
+
+	return False
+
+def scrabble_score(string):
+	"""	Returns the scrabble score from a given string.
+	"""
+
+	string = string.lower()
+	score = 0
+	scores = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3,
+		1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+
+	for char in string:
+		index = ord(char) - ord('a')
+		score += scores[index]
+
+	return score
+
+def max_score(iterable):
+	"""	Returns the maximum scrabble score in an iterable of strings.
+	"""
+
+	max_score = 0
+
+	for word in iterable:
+		max_score += scrabble_score(word)
+
+	return max_score
+
+def seed(seed = ""):
+	"""	Sets the random seed for use in other random functions.
+	"""
+
+	if seed != "":
+		random.seed(seed)
+	else:
+		random.seed()
+
+
+class AnagramMode:
+	""" Creates a class specifically for Anagram Game Mode.
+	"""
+
+	def __init__(self, dictionary):
+		self.dictionary = dictionary
+		self.min_length = 3  # Minimum word length
+		self.max_length = 6  # Maximum word length
+		self.score = 0
+		self.lives = 3
+		self.word = self.random_word(dictionary)
+		self.words = self.find_anagrams(dictionary, self.word)
+		self.max_score = max_score(self.words)
+		self.correct_answers = []
+
+	def is_correct(self, string):
+		"""	Returns True if the word is a valid anagram.
+			Returns False if not a valid anagram.
+		"""
+
+		if string in self.words:
+			self.correct_answers.append(string)
+			self.score += scrabble_score(string)
+
+			if self.score == self.max_score:
+				self.lives = 0
+
+			return True
+
+		self.lives -= 1
+		return False
+
+	def has_guessed(self, string):
+		"""	Returns True if the word has already been guessed:
+			otherwise returns False.
+		"""
+
+		if string in self.correct_answers:
+			return True
+
+		return False
+
+	def is_alive(self):
+		"""	Returns True if it is not game over yet, return false otherwise.
+		"""
+
+		if self.lives > 0:
+			return True
+
+		return False
+
+	def random_word(self, dictionary):
+		"""	Returns a random word from a given dictionary
+			with length in range [min_length, max_length].
+		"""
+
+		word = pick_word(dictionary, random.randrange(len(dictionary)))
+
+		while not self.min_length <= len(word) <= self.max_length\
+			or len(self.find_anagrams(dictionary, word)) < 2:
+			word = pick_word(dictionary, random.randrange(len(dictionary)))
+
+		return word
+
+	def find_anagrams(self, dictionary, string):
+		"""	Returns a list of all anagrams that can be formed from a word
+		from a given dictionary.
+		"""
+
+		anagrams = []
+
+		for word in dictionary:
+			if sorted(word) == sorted(string):
+				anagrams.append(word)
+
+		return anagrams
+
+
+class CombineMode:
+	""" Creates a class specifically for Combine Game Mode.
+	"""
+
+	def __init__(self, dictionary):
+		self.dictionary = dictionary
+		self.min_length = 3  # Min. number of words
+		self.score = 0
+		self.lives = 3
+		self.word = self.random_word(dictionary)
+		self.words = self.find_words(dictionary, self.word)
+		self.correct_answers = []
+		self.max_score = max_score(self.words)
+
+	def is_correct(self, string):
+		"""	Returns True if the word is a valid answer. If not, returns False.
+		"""
+
+		if string in self.words:
+			self.correct_answers.append(string)
+			self.score += scrabble_score(string)
+
+			if self.score == self.max_score:
+				self.lives = 0
+
+			return True
+
+		self.lives -= 1
+		return False
+
+
+	def has_guessed(self, string):
+		"""	Returns True if the word has already been guessed.
+			If not, returns False.
+		"""
+
+		if string in self.correct_answers:
+			return True
+
+		return False
+
+	def is_alive(self):
+		"""	Returns True if it is not game over yet, return false otherwise.
+		"""
+
+		if self.lives > 0:
+			return True
+
+		return False
+
+	def random_word(self, dictionary):
+		"""	Returns a random word from a given dictionary.
+
+			Also makes sure the word can form at least 10
+			words from its letters.
+		"""
+
+		word = random.choice(dictionary)
+
+		while len(word) < 4 or not 5 <= len(self.find_words(dictionary, word)) <= 50:
+			word = random.choice(dictionary)
+
+		return word
+
+	def find_words(self, dictionary, string):
+		"""	Returns all the words that can be formed in a string.
+		"""
+
+		words = []
+
+		for word in dictionary:
+			if check_word(word, string):
+				words.append(word)
+
+		return words
+
+	def combine_words(self, iterable):
+		"""	Returns a shuffled string, combining all strings from an iterable
+			into one string.
+		"""
+
+		min_word = [0] * 26
+		combined_word = ""
+
+		for i in iterable:
+			letter_count = [0] * 26
+
+			for letter in i:
+				index = ord(letter) - ord('a')
+				letter_count[index] += 1
+
+			for j in range(26):
+				if(min_word[j] < letter_count[j]):
+					min_word[j] = letter_count[j]
+
+		for i in range(26):
+			if min_word[i] == 0:
+				continue
+
+			char_key = ord('a') + i
+			combined_word += chr(char_key) * min_word[i]
+
+		_combined_word = [i for i in combined_word]
+		random.shuffle(_combined_word)
+		combined_word = "".join(_combined_word)
+
+		return combined_word
+
+
+
+
---
+++ interface.py
@@ -0,0 +1,201 @@
+import os
+
+
+commands = ['help', 'word', 'quit']
+modes = ['anagram', 'combine']
+modes_short = ['a', 'c']
+
+
+# Game Launch
+ON_LAUNCH = "Hello! Welcome to the Autism Induced Word Unscrambler!\n"
+# Select Game Mode
+ON_SELECT_MODE = """Select a Game Mode!
+	Select the corresponding letter and press enter to start the game.
+
+	> [A]nagram: What else is there!
+	> [C]ombine: Unscramble the puzzle!
+	> [H]olocaust: Coming out Winter 2018!
+	"""
+# Anagram Game Mode Launch
+ON_ANAGRAM_START = """Welcome to Search the Anagram!
+	Try to find all anagrams of the given string.
+
+	E.g. "Reign" -> "Ringe", "Niger",...
+	"""
+# Combine Game Mode Launch
+ON_COMBINE_START = """Welcome to Combine the Words!
+	Try to find all words that can be formed from the string.
+
+	E.g. "Holocaust" -> "Cahoots", "Slouch", "Lust",...
+	"""
+# Game Exit
+ON_EXIT = "Thank you for playing Autism Induced Word Unscrambler!"
+
+def ask(string, iterable):
+		"""	Asks the user for valid input.
+			The input is valid if it is found in the iterable.
+		"""
+
+		while(True):
+			response = input(string).lower().rstrip()
+
+			if response in iterable:
+				return response
+			else:
+				print("Try Again.")
+
+def confirm(string):
+		"""	Prints string and returns True or False depending on user input.
+			Only yes or no decisions are accepted.
+		"""
+
+		decision = ask(string, ('y', 'n', 'yes', 'no'))
+
+		if decision in ('y', 'yes'):
+			return True
+		else:
+			return False
+
+
+class Terminal:
+	"""	Sets up Terminal interface.
+	"""
+
+	def __init__(self):
+		self.running = True
+
+	def is_running(self):
+		if self.running:
+			return True
+
+		return False
+	
+
+	def ask(self, string, iterable):
+		"""	Asks the user for valid input.
+			The input is valid if it is found in the iterable.
+		"""
+
+		while(True):
+			response = input(string).lower().rstrip()
+
+			if response in iterable:
+				return response
+			else:
+				print("Try Again.")
+
+	def confirm(self, string):
+		"""	Prints string and returns True or False depending on user input.
+			Only yes or no decisions are accepted.
+		"""
+
+		decision = self.ask(string, ('y', 'n', 'yes', 'no'))
+
+		if decision in ('y', 'yes'):
+			return True
+		else:
+			return False
+
+	def read_input(self, string = ""):
+		"""	Reads player input and returns data accordingly.
+
+			When a command is input, returns "c_[command_name]".
+			Otherwise, returns the string input.
+		"""
+
+		while True:
+			_string = input(string).lower().rstrip()
+			
+			if _string != "":
+				if _string[0] == "/":
+					if _string[1:] in commands:
+						if _string[1:] == "help":
+							self.help()
+						else:
+							return "c_{}".format(_string[1:])
+					else:
+						print("Unknown command!")
+				else:
+					return _string
+			else:
+				return _string
+			
+	def clear(self):
+		"""	Clears the Terminal Screen
+		"""
+		
+		if os.name == "nt":
+			os.system('cls')
+		else:
+			os.system('clear')
+
+	def start_game(self):
+		"""	Prints text upon opening the game.
+		"""
+
+		print(ON_LAUNCH)
+
+	def select_mode(self):
+		"""	Prints text to select a game mode and returns the user input.
+			Repeatedly prints text to try again if an invalid input is received.
+		"""
+
+		print(ON_SELECT_MODE)
+
+		mode = self.ask("Choose Mode: ", modes + modes_short)
+
+		if mode in ('anagram', 'a'):
+			print(ON_ANAGRAM_START)
+		elif mode in ('combine', 'c'):
+			print(ON_COMBINE_START)
+
+		if mode in modes:
+			return modes[modes.index(mode)]
+		else:
+			return modes[modes_short.index(mode)]
+
+	def help(self):
+		"""	Prints help.
+		"""
+		string = "Commands: "
+
+		for word in commands:
+			string += "  /{}".format(word)
+
+		print(string)
+
+	def has_guessed(self):
+		"""	Prints text that you already guessed that word.
+		"""
+
+		print("You have already guessed that word! Try again.")
+
+	def on_correct(self, score):
+		"""	Triggers when a valid answer is made
+		"""
+
+		print("Correct! Score: {}".format(score))
+
+	def on_mistake(self, retries):
+		"""	Triggers when an invalid answer is made.
+		"""
+
+		print("Wrong! Retries left: {}".format(retries))
+
+	def chosen_word(self, string):
+		"""	Prints the chosen word.
+		"""
+
+		print("The word is: {}".format(string))
+
+	def calculate_score(self, score, max_score):
+		"""	Prints score.
+		"""
+
+		print("Your score is: {}/{}".format(score, max_score))
+
+	def on_exit(self):
+		"""	Prints text on exit.
+		"""
+			
+		print(ON_EXIT)
